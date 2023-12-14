b6a37d2cb4e6501f4bb8da484dd3e8298a38df45
didukhroma <didukhroma@gmail.com>
2023-12-14 15:34:59 +0200

    add: change logic

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -18,27 +18,33 @@ const markup = galleryItems
     </li>`
   )
   .join("");
-// Modal
-const createModal = (link) =>
-  basicLightbox.create(
-    `
-      <img src="${link}" width="800" height="600">
-  `
-  );
+
 // Click handler
 const handleClick = (event) => {
   event.preventDefault();
   if (event.target.nodeName !== "IMG") return;
-
   const link = event.target.dataset.source;
 
-  const modal = createModal(link);
-  // Key handler
-  const handleKey = (event) =>
-    event.key === "Escape" &&
-    modal.close(() => document.removeEventListener("keydown", handleKey));
-  // Show modal
-  modal.show(() => document.addEventListener("keydown", handleKey));
+  const opt = {
+    onShow: () => {
+      document.addEventListener("keydown", handleKeyboard);
+    },
+    onClose: () => {
+      document.removeEventListener("keydown", handleKeyboard);
+    },
+  };
+
+  const instance = basicLightbox.create(
+    `
+      <img src="${link}" width="800" height="600">
+  `,
+    opt
+  );
+
+  const handleKeyboard = (e) =>
+    instance.visible() && e.key === "Escape" && instance.close();
+
+  instance.show();
 };
 
 if (!galleryRef) console.error("Gallery not found");
